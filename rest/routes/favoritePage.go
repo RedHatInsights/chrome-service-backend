e115b27fa7aac9a8b90764c155567c362f7eb3d5
Alexander Rivera <xanverapr@gmail.com>
2022-08-17 09:19:19 -0400

    Adding default value to user, fixed params error

--- rest/routes/favoritePage.go
+++ rest/routes/favoritePage.go
@@ -1,11 +1,12 @@
 package routes
 
 import (
+	"fmt"
+	"strconv"
 	"net/http"
   "encoding/json"
   "github.com/go-chi/chi/v5"
   
-  // "github.com/RedHatInsights/chrome-service-backend/rest/database"
   "github.com/RedHatInsights/chrome-service-backend/rest/models"
   "github.com/RedHatInsights/chrome-service-backend/rest/service"
 	"github.com/RedHatInsights/chrome-service-backend/rest/util"
@@ -14,13 +15,19 @@ import (
 func GetFavoritePage(w http.ResponseWriter, r *http.Request) {
   user := r.Context().Value(util.USER_CTX_KEY).(models.UserIdentity)
   userID := user.ID
-  searchParams := r.URL.Query().Get("archived")
+  searchParams, boolParamsErr := strconv.ParseBool(
+  	r.URL.Query().Get(util.ARCHIVED_PARAM))
 	var userFavoritePages []models.FavoritePage
 	var err error
+	fmt.Println("Testing out our searchParams: ", searchParams)
 
-  if (searchParams == util.ARCHIVED_PARAM) {
+	if boolParamsErr != nil { // graceful error handling in to-do list
+		w.Write([]byte("There is a problem in your requests parameters. Please refer to docs"))
+	}
+
+  if (searchParams) {
     w.Write([]byte("These are no longer nice, no favorited no more"))  
-  } else if (searchParams == util.DEFAULT_PARAM) {
+  } else if (!searchParams) {
     userFavoritePages, err = service.GetUserActiveFavoritePages(userID) 
   }
   

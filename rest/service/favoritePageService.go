89816f6502468b28a16ce842ea9366583106f211
Martin Maroši <marvusm.mmi@gmail.com>
2023-01-20 09:22:58 +0100

    Simplify favorite page update.

--- rest/service/favoritePageService.go
+++ rest/service/favoritePageService.go
@@ -1,84 +1,70 @@
 package service
 
 import (
-  "time"
-
-  "github.com/RedHatInsights/chrome-service-backend/rest/models"
-  "github.com/RedHatInsights/chrome-service-backend/rest/database"
+	"github.com/RedHatInsights/chrome-service-backend/rest/database"
+	"github.com/RedHatInsights/chrome-service-backend/rest/models"
 )
 
-func GetUserActiveFavoritePages(userID uint)([]models.FavoritePage, error) {
-  var activeFavoritePages []models.FavoritePage
-  
-  err := database.DB.Where("user_identity_id = ?", userID).Where("favorite", true).Find(&activeFavoritePages).Error
-  
-  return activeFavoritePages, err
+func GetUserActiveFavoritePages(userID uint) ([]models.FavoritePage, error) {
+	var activeFavoritePages []models.FavoritePage
+
+	err := database.DB.Where("user_identity_id = ?", userID).Where("favorite", true).Find(&activeFavoritePages).Error
+
+	return activeFavoritePages, err
 }
 
-func GetAllUserFavoritePages(userID uint)([]models.FavoritePage, error) {
-  var favoritePages []models.FavoritePage
+func GetAllUserFavoritePages(userID uint) ([]models.FavoritePage, error) {
+	var favoritePages []models.FavoritePage
 
-  err := database.DB.Where("user_identity_id = ?", userID).Find(&favoritePages).Error
-  return favoritePages, err
+	err := database.DB.Where("user_identity_id = ?", userID).Find(&favoritePages).Error
+	return favoritePages, err
 }
 
-func GetUserArchivedFavoritePages(userID uint)([]models.FavoritePage, error) {
-  var archivedFavorites []models.FavoritePage
+func GetUserArchivedFavoritePages(userID uint) ([]models.FavoritePage, error) {
+	var archivedFavorites []models.FavoritePage
 
-  err := database.DB.
-    Where("user_identity_id = ?", userID).
-    Where("favorite = ?", false).
-    Find(&archivedFavorites).
-    Error
+	err := database.DB.
+		Where("user_identity_id = ?", userID).
+		Where("favorite = ?", false).
+		Find(&archivedFavorites).
+		Error
 
-    return archivedFavorites, err
+	return archivedFavorites, err
 }
 
-func CheckIfExistsInDB(allFavoritePages []models.FavoritePage, newFavoritePage models.FavoritePage)(bool) {
-  pageExists := false
-  
-  for _, page := range allFavoritePages {
-    if page.Pathname == newFavoritePage.Pathname {
-      pageExists = true
-      break
-    }
-  }
+func CheckIfExistsInDB(allFavoritePages []models.FavoritePage, newFavoritePage models.FavoritePage) bool {
+	pageExists := false
 
-  return pageExists
-} 
+	for _, page := range allFavoritePages {
+		if page.Pathname == newFavoritePage.Pathname {
+			pageExists = true
+			break
+		}
+	}
 
-func UpdateFavoritePage(favoritePage models.FavoritePage)(error) {
-  var currentPage models.FavoritePage
-
-  err := database.DB.Where("pathname = ?", favoritePage.Pathname).First(&currentPage).Error
-  
-  if err != nil {
-    return err
-  }
-  
-  return database.DB.Model(&currentPage).
-    Update("favorite", favoritePage.Favorite).
-    Update("updated_at", time.Now()).
-    Error
+	return pageExists
 }
 
-func SaveUserFavoritePage(userID uint, newFavoritePage models.FavoritePage)(error) {
-  var userFavoritePages []models.FavoritePage
-
-  userFavoritePages, err := GetAllUserFavoritePages(userID)
-
-  if err != nil {
-    panic(err)
-  }
-
-  alreadyInDB := CheckIfExistsInDB(userFavoritePages, newFavoritePage)
-
-  if alreadyInDB {
-    err = UpdateFavoritePage(newFavoritePage)
-  } else {
-    err = database.DB.Create(&newFavoritePage).Error
-  }
- 
-  return err
+func UpdateFavoritePage(favoritePage models.FavoritePage) error {
+	return database.DB.Model(&models.FavoritePage{}).Where("pathname = ?", favoritePage.Pathname).Update("favorite", favoritePage.Favorite).Error
 }
 
+func SaveUserFavoritePage(userID uint, newFavoritePage models.FavoritePage) error {
+	var userFavoritePages []models.FavoritePage
+
+	userFavoritePages, err := GetAllUserFavoritePages(userID)
+
+	if err != nil {
+		panic(err)
+	}
+
+	alreadyInDB := CheckIfExistsInDB(userFavoritePages, newFavoritePage)
+
+	if alreadyInDB {
+		err = UpdateFavoritePage(newFavoritePage)
+	} else {
+		err = database.DB.Create(&newFavoritePage).Error
+	}
+
+	return err
+}

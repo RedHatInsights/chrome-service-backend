1531d5a284b38eb90bd63f0d06f3824d19abec2c
Martin Maroši <marvusm.mmi@gmail.com>
2023-02-28 10:09:17 +0100

    Improve database migration script.

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -6,15 +6,38 @@ import (
 	"github.com/RedHatInsights/chrome-service-backend/rest/models"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 func main() {
 	godotenv.Load()
 	config.Init()
 	database.Init()
-	err := database.DB.AutoMigrate(&models.FavoritePage{}, &models.LastVisitedPage{}, &models.SelfReport{}, &models.UserIdentity{}, &models.ProductOfInterest{})
+
+	var bundleRes *gorm.DB
+
+	// Wrap DB migration into a single transaction
+	err := database.DB.Transaction(func(tx *gorm.DB) error {
+		err := tx.AutoMigrate(&models.FavoritePage{}, &models.LastVisitedPage{}, &models.SelfReport{}, &models.UserIdentity{}, &models.ProductOfInterest{})
+		if err != nil {
+			return err
+		}
+		// Migrate all nil visited_bundles to empty object
+		bundleRes = database.DB.Model(&models.UserIdentity{}).Where("visited_bundles IS NULL").Update("visited_bundles", []byte(`{}`))
+		if bundleRes.Error != nil {
+			return bundleRes.Error
+		}
+
+		return nil
+	})
+
 	if err != nil {
+		logrus.Error("Unable to migrate database!")
 		panic(err)
+	}
+
+	if bundleRes.RowsAffected > 0 {
+		logrus.Infof("Migrated %d user identity bundles rows", bundleRes.RowsAffected)
 	}
 	logrus.Info("Migration complete")
 }

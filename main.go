111922359e4e663c069726b494cebce22f1ff435
Blake Holifield <bholifie@redhat.com>
2023-03-13 11:09:07 -0400

    remove custom metric from route and dashboard

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	router.Get("/health", HealthProbe)
 
-	router.With(routes.PrometheusMiddleware).Route("/api/chrome-service/v1/", func(subrouter chi.Router) {
+	router.Route("/api/chrome-service/v1/", func(subrouter chi.Router) {
 		subrouter.Use(m.ParseHeaders)
 		subrouter.Use(m.InjectUser)
 		subrouter.Get("/hello-world", HelloWorld)

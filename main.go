d85e88be3c1a36bdd2bfcad3be7478f8811957e7
Blake Holifield <bholifie@redhat.com>
2023-04-06 14:19:15 -0400

    add entrypoints to learning resources and update nav items

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	router.Get("/health", HealthProbe)
 	fs := http.FileServer(http.Dir("./static/"))
 
-	// can't be in sub router as we don't enforce indentity header
+	// can't be in sub router as we don't enforce identity header
 	router.Handle("/api/chrome-service/v1/static/*", http.StripPrefix("/api/chrome-service/v1/static", fs))
 
 	router.Route("/api/chrome-service/v1/", func(subrouter chi.Router) {

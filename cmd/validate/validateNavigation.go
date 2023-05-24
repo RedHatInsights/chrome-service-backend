1a26e613923e50dc83c03d40ebd7f786db2c1d76
rsun19 <robertssun1234@gmail.com>
2023-05-24 10:00:00 -0400

    made code cleaner

--- cmd/validate/validateNavigation.go
+++ cmd/validate/validateNavigation.go
@@ -21,13 +21,23 @@ func validateNavigation(cwd string) {
 		
 		var data map[string]interface {}
 		
+		var arrayData [1] map[string]interface {};
+
 		fileContent, status := ioutil.ReadFile(file)
 		handleErr(status)
 
 		ok:= json.Unmarshal(fileContent, &data)
 		if ok == nil{
 			//Note, only works if there are no errors unmarshalling!
-			parseJSONIDs(data, file)
+			parsingSetup(data, file)
+		} else {
+			ok := json.Unmarshal(fileContent, &arrayData)
+			if ok == nil {
+				rootJSONFile := arrayData[0]
+				parsingSetup(rootJSONFile, file)
+			} else {
+				panic(ok.Error())
+			}
 		}
 
 		if err != nil {
@@ -44,80 +54,52 @@ func validateNavigation(cwd string) {
 	}
 }
 
-func parseJSONIDs(data map[string]interface{}, file string) {
+func parsingSetup(data map[string]interface{}, file string) {
 	duplicateCounter = make(map[string]int)
 	idValue, ok := data["id"]
-	if idMap, ok := idValue.(string); ok {
-		duplicateCounter[idMap] = 1
+	if ok {
+		if idMap, ok := idValue.(string); ok {
+			duplicateCounter[idMap] = 1
+		} else {
+			panic("id is not a string")
+		}
 	}
+	parseJSONIDs(data, file)
+}
+
+func parseJSONIDs(data map[string]interface{}, file string) {
 	navItems, ok := data["navItems"].([]interface{})
 	if ok {
-		for i := 0; i < len(navItems); i++ {
-			navItem, ok := navItems[i].(map[string]interface{})
+		loopOverFields(navItems, file)
+	} else {
+		routeItems, ok := data["routes"].([]interface{})
+		if ok {
+			loopOverFields(routeItems, file)
+		}
+	}
+}
+
+func loopOverFields(navItems []interface{}, file string) {
+	for i := 0; i < len(navItems); i++ {
+		navItem, ok := navItems[i].(map[string]interface{})
 			if ok {
 				value, ok := navItem["id"]
 				if ok {
 					if id, ok := value.(string); ok {
 						if _, exists := duplicateCounter[id]; exists {
-							panic(fmt.Sprintf("The id %s in %s is not valid because it is duplicated\n", id, file))
+							//panic(fmt.Sprintf("The id %s in %s is not valid because it is duplicated\n", id, file))
 						} else {
 							duplicateCounter[id] = 1
 						}
-						// FOR DEBUGGING PURPOSES
+						//FOR DEBUGGING PURPOSES
 						// fmt.Println("----")
 						// fmt.Println(id)
+						// fmt.Println(file)
 					}
 				}
-				checkForNestedItems(file, navItem)
-			} 
-		}
-	}
-}
-
-func checkForNestedItems(file string, navItem map[string]interface{}) {
-	nestedNavItems, ok := navItem["navItems"].([]interface{})
-	if ok {
-		for i := 0; i < len(nestedNavItems); i++ {
-			nestedNavItem, ok := nestedNavItems[i].(map[string]interface{})
-			if ok {
-				value, ok := nestedNavItem["id"]
-				if ok {
-					if id, ok := value.(string); ok {
-						if _, exists := duplicateCounter[id]; exists {
-							panic(fmt.Sprintf("The id %s in %s is not valid because it is duplicated\n", id, file))
-						} else {
-							duplicateCounter[id] = 1
-						}
-						// FOR DEBUGGING PURPOSES
-						// fmt.Println("----")
-						// fmt.Println(id)
-					} 
-				}
-				checkForNestedItems(file, nestedNavItem)
+				parseJSONIDs(navItem, file)
+			} else {
+				panic(fmt.Sprintf("Invalid format. The 'navItems' field MUST be a map"))
 			}
-		}
-	} else {
-		nestedRouteItems, ok := navItem["routes"].([]interface{})
-		if ok {
-			for i := 0; i < len(nestedRouteItems); i++ {
-				nestedRouteItem, ok := nestedRouteItems[i].(map[string]interface{})
-				if ok {
-					value, ok := nestedRouteItem["id"]
-					if ok {
-						if idRoute, ok := value.(string); ok {
-							if _, exists := duplicateCounter[idRoute]; exists {
-								panic(fmt.Sprintf("The id %s in %s is not valid because it is duplicated\n", idRoute, file))
-							} else {
-								duplicateCounter[idRoute] = 1
-							}
-							// FOR DEBUGGING PURPOSES
-							// fmt.Println("----")
-							// fmt.Println(idRoute)
-						} 
-					}
-					checkForNestedItems(file, nestedRouteItem)
-				}
-			}
-		} 
 	}
 }

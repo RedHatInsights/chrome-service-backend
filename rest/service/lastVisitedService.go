53085b0902341d559857f4a872315b82763fc337
Blake Holifield <bholifie@redhat.com>
2024-01-17 14:08:48 -0500

    use Gorm's batch insert

--- rest/service/lastVisitedService.go
+++ rest/service/lastVisitedService.go
@@ -16,19 +16,23 @@ func GetUsersLastVisitedPages(accountId uint) ([]models.LastVisitedPage, error) 
 // HandlePostLastVisitedPages inserts the most recent pages from chrome. Once they are added,
 // all older entries are removed from the table.
 func HandlePostLastVisitedPages(recentPages []models.LastVisitedPage, accountId uint) error {
+	// change the ID in place to avoid a user with ID 0
+	for k, _ := range recentPages {
+		recentPages[k].UserIdentityID = accountId
+	}
+	if err := database.DB.Create(&recentPages).Error; err != nil {
+		logrus.Debugf("Pages to be inserted: %+v\n", recentPages)
+		// If we encounter an error, we want to bail out of the deletion as well. Extra pages will be
+		// corrected by a subsequent successful call
+		return err
+	}
 	var ids []uint
+	// Unique ids are given to the entry from Gorm AFTER they are successfully inserted
 	for _, v := range recentPages {
-		v.UserIdentityID = accountId
-		if err := database.DB.Create(&v).Error; err != nil {
-			// If we encounter an error, we want to bail out of the deletion as well. Extra pages will be
-			// corrected by a subsequent successful call
-			return err
-		}
-		// Unique ids are given to the entry from Gorm AFTER they are successfully inserted
 		ids = append(ids, v.ID)
 	}
-	logrus.Debugf("%+v\n", recentPages)
-	logrus.Debugf("%+v\n", ids)
+	logrus.Debugf("Pages to be inserted: %+v\n", recentPages)
+	logrus.Debugf("Inserted IDs: %+v\n", ids)
 	// Since we have all the IDs of the newly added pages, we can remove all other pages
 	err := database.DB.Unscoped().Where("user_identity_id = ?", accountId).Where("id NOT IN ?", ids).Delete(&models.LastVisitedPage{}).Error
 	if err != nil {

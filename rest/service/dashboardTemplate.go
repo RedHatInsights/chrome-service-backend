79054a5b6797dde8ca30410585cdbc951e3618b1
Cody Mitchell <comitche@redhat.com>
2024-04-25 13:42:55 -0400

    sets subs widget default width to 4

--- rest/service/dashboardTemplate.go
+++ rest/service/dashboardTemplate.go
@@ -168,7 +168,7 @@ func init() {
 		WidgetMapping[models.Subscriptions] = models.ModuleFederationMetadata{
 			Scope:    "subscriptionInventory",
 			Module:   "./SubscriptionsWidget",
-			Defaults: models.BaseWidgetDimensions.InitDimensions(models.BaseWidgetDimensions{}, 1, 3, 5, 1),
+			Defaults: models.BaseWidgetDimensions.InitDimensions(models.BaseWidgetDimensions{}, 4, 3, 5, 1),
 			Config: models.WidgetConfiguration{
 				HeaderLink: models.WidgetHeaderLink{
 					Title: "Manage subscriptions",

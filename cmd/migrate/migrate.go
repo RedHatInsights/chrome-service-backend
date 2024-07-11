a17f3ecc148a7b57008c92e3235a8e15468a7471
Blake Holifield <bholifie@redhat.com>
2024-07-11 09:38:17 -0400

    remove dashboard migrations

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -2,70 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/RedHatInsights/chrome-service-backend/rest/database"
 	"github.com/RedHatInsights/chrome-service-backend/rest/models"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
-	"gorm.io/datatypes"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 	"gorm.io/gorm/logger"
 )
-
-func getValidGridItems(items []models.GridItem) []models.GridItem {
-	newGridItems := []models.GridItem{}
-	for _, item := range items {
-		widgetTypes := strings.Split(item.ID, "#")
-		if len(widgetTypes) > 0 {
-			widgetType := widgetTypes[0]
-			err := models.AvailableWidgets(widgetType).IsValid()
-			if err == nil {
-				newGridItems = append(newGridItems, item)
-			} else {
-				logrus.Infof("Removing invalid widget type: %s", widgetType)
-			}
-		}
-	}
-
-	return newGridItems
-}
-
-func migrateDashboardWidgets(tx *gorm.DB) *gorm.DB {
-	var templates []models.DashboardTemplate
-	res := tx.Find(&templates)
-	if res.Error != nil {
-		return res
-	}
-
-	logrus.Infoln("transform grid items into gorm json type")
-	for _, t := range templates {
-		t.TemplateConfig.Xl = datatypes.NewJSONType(getValidGridItems(t.TemplateConfig.Xl.Data()))
-		t.TemplateConfig.Lg = datatypes.NewJSONType(getValidGridItems(t.TemplateConfig.Lg.Data()))
-		t.TemplateConfig.Md = datatypes.NewJSONType(getValidGridItems(t.TemplateConfig.Md.Data()))
-		t.TemplateConfig.Sm = datatypes.NewJSONType(getValidGridItems(t.TemplateConfig.Sm.Data()))
-		res = tx.Save(&t)
-
-		if res.Error != nil {
-			return res
-		}
-	}
-
-	templates = []models.DashboardTemplate{}
-
-	fmt.Println("Removing all non default templates")
-	// remove all non default templates
-	res = tx.Unscoped().Debug().Clauses(clause.Locking{
-		Strength: "SHARE",
-		Options:  "NOWAIT",
-	}).Where(`"default" = ?`, false).Delete(&[]models.DashboardTemplate{})
-	if res.Error != nil {
-		return res
-	}
-
-	return res
-}
 
 func main() {
 	godotenv.Load()
@@ -73,7 +16,6 @@ func main() {
 
 	var bundleRes *gorm.DB
 	var visitedRes *gorm.DB
-	var dashboardRes *gorm.DB
 	tx := database.DB.Begin().Session(&gorm.Session{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
@@ -107,16 +49,6 @@ func main() {
 		err := tx.Where(`"favorite" = ?`, false).Unscoped().Delete(&models.FavoritePage{}).Error
 		if err != nil {
 			fmt.Println("Unable to migrate database!", err)
-			tx.Rollback()
-			panic(err)
-		}
-	}
-
-	fmt.Println("Removing dashboard templates sx variant")
-	// temporary - removes unused typo column in dashboard template tables
-	if tx.Migrator().HasColumn(&models.DashboardTemplate{}, "sx") {
-		if err := tx.Migrator().DropColumn(&models.DashboardTemplate{}, "sx"); err != nil {
-			fmt.Println("Unable to migrate database!", err.Error())
 			tx.Rollback()
 			panic(err)
 		}
@@ -195,21 +127,6 @@ func main() {
 		fmt.Println("Unable to migrate database!", bundleRes.Error.Error())
 		tx.Rollback()
 		panic(bundleRes.Error)
-	}
-
-	dashboardRes = migrateDashboardWidgets(tx)
-	if dashboardRes.Error != nil {
-		fmt.Println("Unable to migrate database!", dashboardRes.Error.Error())
-		tx.Rollback()
-		panic(dashboardRes.Error)
-	}
-
-	err := tx.Commit().Error
-
-	if err != nil {
-		fmt.Println("Unable to migrate database!", err.Error())
-		tx.Rollback()
-		panic(err)
 	}
 
 	if bundleRes.RowsAffected > 0 {
